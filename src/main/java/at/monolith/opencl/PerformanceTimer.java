4a92a0fce13bf11208779becb5918cef2b845217
David Mueller <david.mueller@tuwien.ac.at>
2016-07-15 16:29:30 +0200

    Added performance timer.

---
+++ src/main/java/at/monolith/opencl/PerformanceTimer.java
@@ -0,0 +1,23 @@
+package at.monolith.opencl;
+
+/**
+ * Created by David on 20.03.2016.
+ */
+public class PerformanceTimer {
+
+	private long time = 0;
+	public boolean active = false;
+
+	public void reset() {
+		time = System.nanoTime();
+	}
+
+	public void lap(String s) {
+		if(active) {
+			long delta = (System.nanoTime() - time) / (1000 * 1000);
+			System.out.println(s + " " + delta + "ms");
+			reset();
+		}
+	}
+
+}
